35b8ef93f1aea927d7fa7aba4fdec25c7941b931
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-10 00:20:19 +0600

    update code

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
